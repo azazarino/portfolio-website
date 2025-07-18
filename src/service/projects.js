ed0bc7ff4e9b8748001e8f84404cb443734ca6e9
azazarino <azeromar2002@gmail.com>
2025-07-18 22:35:48 +0200

    added fade in animation to modals

--- src/service/projects.js
+++ src/service/projects.js
@@ -1,12 +1,11 @@
 export const projectList = [
-    {
-        "id": 1,
-        "title": "TSIU03 project course",
-        "description": "Built an oscilloscope in VHDL that displays the left and right sounds signal on a VGA screen",
-        "full_description" :`This project invovlved building a simple oscilloscope that display the left and right ADC values on a VGA screen.
-        This required low-level hardware interfacing, clock division, ADC integration and careful timing coordination.`,
-        "skills": ["test1", "skill2"]
-        
+  {
+    id: 1,
+    title: "TSIU03 project course",
+    description: "Built an oscilloscope in VHDL that displays the left and right sound signals on a VGA screen",
+    full_description: `This project involved building a simple oscilloscope that displays the left and right ADC values on a VGA screen.
+    This required low-level hardware interfacing, clock division, ADC integration, and careful timing coordination.`,
+    skills: ["test1", "skill2"]
     },
     {
         "id": 2,
